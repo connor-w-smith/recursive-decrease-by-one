da38e534520f7326ccc594acbd7764527ebacbad
connor-w-smith <connorwadesmith@gmail.com>
2025-04-24 09:04:28 -0500

    Edited the findmax function
    I realized that the function needs to print the steps, so I refactored and added some steps.

--- example.cpp
+++ example.cpp
@@ -14,15 +14,24 @@ int findMaximum(int arr[], int size) {
     
     // The base case will return if there's only one element
     if (size == 1) {
+        cout << "Base Case: Returning " << arr[0] << endl;
         return arr[0];
     }
 
     // This is the recursive case. Meaning it will find the maximum in the rest of the array.
     int maxOfFunc = findMaximum(arr, size -1);
+    int curr = arr[size -1];
 
-    // Now we compare and return the larger number between the last element and maxOfFunc
-    // Also the use of '?' and ':'. These are representations of conditional operators, shorthand for an if else statement. AI was used to help with this statement below.
-    return (arr[size -1] > maxOfFunc) ? arr[size - 1] : maxOfFunc;
+     // Also the use of '?' and ':'. 
+     // These are representations of conditional operators, shorthand for an if else statement. AI was used to help with this statement below.
+    int result = (curr > maxOfFunc) ? curr : maxOfFunc;
+
+    // Printing the comparisons
+    cout << "Comparing arr[" << size - 1 << "] = " << curr;
+    cout << " with maxOfFunc = " << maxOfFunc;
+    cout << " => = " << result << endl;
+
+    return result;
 }
 
 
@@ -31,6 +40,6 @@ int main() {
     int size = sizeof(S) / sizeof(S[0]);
 
     int maxElement = findMaximum(S, size);
-    cout << "The array given is {3, 5, 2, 9, 1, 8, 0, 2}"
+    cout << "The array given is {3, 5, 2, 9, 1, 8, 0, 2}";
     cout << "The maximum element in the array is: " << maxElement << endl;
 }
